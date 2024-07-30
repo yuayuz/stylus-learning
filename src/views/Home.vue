91b9f17a749e6d52b46b82b0fd1bc90f874feda6
yu <2665764951@qq.com>
2024-07-30 16:18:44 +0800

    add `Home`

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import router from '@/router'
+
+const handleRouter = function (page: string) {
+  router.push({ name: page })
+}
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style scoped>
+@import '@/stylus/example.css';
+</style>
